6b6a55e9ffee4082a8df16b04389921fed86edf0
suzuki <>
2020-04-06 13:33:17 +0900

    add houselogo&contacttext, change favicon&ogpimg

---
+++ 本番/assets/js/slide.js
@@ -0,0 +1,45 @@
+const sliderImages_fv = document.querySelectorAll('.slide_fv');
+const sliderImages_gl = document.querySelectorAll('.slide_gl');
+
+let currentFv = 0;
+const arrayFv = sliderImages_fv.length - 1;
+
+let currentGl = 0;
+const arrayGl = sliderImages_gl.length - 1;
+
+
+
+function slide_fv() {
+	if(currentFv < arrayFv){
+		sliderImages_fv[currentFv + 1].style.display = 'block';
+		sliderImages_fv[currentFv].style.display = 'none';
+		currentFv += 1;
+	} else if (currentFv = arrayFv){
+		sliderImages_fv[0].style.display = 'block';
+		sliderImages_fv[currentFv].style.display = 'none';
+		currentFv = 0;
+	}
+}
+
+
+function slide_gl_auto() {
+	if(currentGl < arrayGl){
+		sliderImages_gl[currentGl + 1].style.display = 'block';
+		sliderImages_gl[currentGl].style.display = 'none';
+		currentGl += 1;
+	} else if (currentGl = arrayGl){
+		sliderImages_gl[0].style.display = 'block';
+		sliderImages_gl[currentGl].style.display = 'none';
+		currentGl = 0;
+	}
+}
+
+
+
+setInterval(slide_fv, 2000);
+
+setInterval(slide_gl_auto, 2000);
+
+// document.addEventListener('DOMContentLoaded', gallerySwitch(), false);
+
+
